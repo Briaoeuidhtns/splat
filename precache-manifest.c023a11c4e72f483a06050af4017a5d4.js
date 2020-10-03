eec40b765934c7462cae8a192ddb01bf3df9de9d
Brian Brunner <brian.brunner.bb@gmail.com>
2020-10-02 21:59:52 -0600

    Updates

---
+++ precache-manifest.c023a11c4e72f483a06050af4017a5d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0610c9633413d33b40c5101f412df91d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba847c8bfbae8a953799",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e8cf64a7fffe5d89b73a",
+    "url": "/static/js/2.99ef3a9a.chunk.js"
+  },
+  {
+    "revision": "62968af45c11022646908ac6174576d5",
+    "url": "/static/js/2.99ef3a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba847c8bfbae8a953799",
+    "url": "/static/js/main.ac171832.chunk.js"
+  },
+  {
+    "revision": "6762d357d3fc7a8eb624",
+    "url": "/static/js/runtime-main.8e41fdbd.js"
+  }
+]);
